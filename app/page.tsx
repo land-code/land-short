075da81037befc7c86fdc3e9eff216f88ac719a7
lfainrdst01 <131370712+lfainrdst01@users.noreply.github.com>
2023-11-02 16:31:14 +0000

    Add refresh button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { revalidatePath } from "next/cache";
 import ShortLinks from "./components/short-links";
 import ShortenerForm from "./components/shortener-form";
+import { refresh } from "./components/refresh";
 
 
 export default async function Home() {
@@ -10,7 +12,12 @@ export default async function Home() {
         <ShortenerForm />
       </section>
       <section className="flex flex-col items-center max-w-full">
-        <h2 className="text-3xl">Saved links</h2>
+        <div className="flex items-center gap-4">
+          <h2 className="text-3xl">Saved links</h2>
+          <form action={refresh}>
+            <button type="submit" className="bg-zinc-800 text-zinc-200 p-2 rounded-xl hover:bg-zinc-500 active:bg-zinc-400">Refresh</button>
+          </form>
+        </div>
         <ShortLinks />
       </section>
     </div>
