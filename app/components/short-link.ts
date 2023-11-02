a74544268daedd8ad4977d8b04356b75b7875a26
lfainrdst01 <131370712+lfainrdst01@users.noreply.github.com>
2023-11-02 15:55:44 +0000

    Return errors and pending ocurred when shorthing link

--- app/components/short-link.ts
+++ app/components/short-link.ts
@@ -6,18 +6,24 @@ import { cookies } from "next/headers"
 import { isValidUrl } from "../utils/is-valid-url"
 import { revalidatePath, revalidateTag } from "next/cache"
 
-export const shortLink = async (formData: FormData) => {
-  const supabase = createServerActionClient<Database>({ cookies })
-  const longLink = formData.get('long-link') as string | null || ''
-  const name = formData.get('username') as string | null || ''
-  if (!longLink === null && !name) return { message: 'Name and long link not passed'}
-  const { error } = await supabase
-    .from('short_codes')
-    .insert({
-      content: longLink,
-      is_url: isValidUrl(longLink),
-      name,
-    })
-  if (error) console.error (error)
-  revalidatePath('', 'page')
+export const shortLink = async (prevState: any, formData: FormData) => {
+  try {
+    const supabase = createServerActionClient<Database>({ cookies })
+    const longLink = formData.get('long-link') as string | null || ''
+    const name = formData.get('username') as string | null || ''
+    if (!longLink === null && !name) return { message: 'Name and long link not passed'}
+    const { error } = await supabase
+      .from('short_codes')
+      .insert({
+        content: longLink,
+        is_url: isValidUrl(longLink),
+        name,
+      })
+    if (error) console.error (error)
+    revalidatePath('', 'page')
+  } catch (err) {
+    return {
+      message: 'Failed to short'
+    }
+  }
 }
--- app/components/shortener-form.tsx
+++ app/components/shortener-form.tsx
@@ -1,8 +1,23 @@
+'use client'
+
 import { shortLink } from "./short-link"
+import { useFormState, useFormStatus } from "react-dom"
+
+const initialValue = {
+  message: null
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus()
+  return (
+    <button type="submit" aria-disabled={pending} className="bg-zinc-200 text-zinc-800 p-2 rounded-xl hover:bg-zinc-400 active:bg-zinc-500">Short</button>
+  )
+}
 
 export default function ShortenerForm() {
+  const [state, formAction] = useFormState(shortLink, initialValue)
   return (
-    <form action={shortLink} className="bg-zinc-800 text-zinc-200 p-4 rounded-xl flex flex-col items-center gap-2 w-[500px] max-w-full">
+    <form action={formAction} className="bg-zinc-800 text-zinc-200 p-4 rounded-xl flex flex-col items-center gap-2 w-[500px] max-w-full">
       <label className="w-full">
         <span>
           Link or code:
@@ -16,6 +31,7 @@ export default function ShortenerForm() {
         <input required name="username" className="block p-2 rounded-xl bg-zinc-600 text-zinc-200 w-full" placeholder="Introduce your username" />
       </label>
       <button type="submit" className="bg-zinc-200 text-zinc-800 p-2 rounded-xl hover:bg-zinc-400 active:bg-zinc-500">Short</button>
+      <p>{state?.message}</p>
     </form>
   )
 }
