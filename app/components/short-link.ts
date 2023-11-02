18ad2394e57d6fb04a695798989ab31c5048cf86
lfainrdst01 <131370712+lfainrdst01@users.noreply.github.com>
2023-11-02 15:40:20 +0000

    Wait until supabase inserts the content

--- app/components/short-link.ts
+++ app/components/short-link.ts
@@ -4,17 +4,20 @@ import { Database } from "@/database.types"
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 import { isValidUrl } from "../utils/is-valid-url"
+import { revalidatePath, revalidateTag } from "next/cache"
 
 export const shortLink = async (formData: FormData) => {
-    const supabase = createServerActionClient<Database>({ cookies })
-    const longLink = formData.get('long-link') as string | null || ''
-    const name = formData.get('username') as string | null || ''
-    if (!longLink === null && !name) return { message: 'Name and long link not passed'}
-    supabase
-      .from('short_codes')
-      .insert({
-        content: longLink,
-        is_url: isValidUrl(longLink),
-        name,
-      })
-  }
+  const supabase = createServerActionClient<Database>({ cookies })
+  const longLink = formData.get('long-link') as string | null || ''
+  const name = formData.get('username') as string | null || ''
+  if (!longLink === null && !name) return { message: 'Name and long link not passed'}
+  const { error } = await supabase
+    .from('short_codes')
+    .insert({
+      content: longLink,
+      is_url: isValidUrl(longLink),
+      name,
+    })
+  if (error) console.error (error)
+  revalidatePath('', 'page')
+}
