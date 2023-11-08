3a07d79c0c28827157dba8c3f2068df5ba3f85ed
lfainrdst01 <131370712+lfainrdst01@users.noreply.github.com>
2023-11-08 14:49:02 +0000

    Pending changes exported from your codespace

--- app/[lang]/login-logout-button.tsx
+++ app/[lang]/login-logout-button.tsx
@@ -3,6 +3,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 import Link from "next/link"
 import { logout } from "@/app/actions/logout"
+import { materialSymbols } from "../fonts/material-symbols"
 
 const LoginLogoutButton = async ({ dictionary }: {
   dictionary: {
@@ -31,8 +32,9 @@ const LoginLogoutButton = async ({ dictionary }: {
               <p className="text-zinc-200 text-lg">{user.email}</p>
               <form action={logout}>
                 <button
-                  className="bg-zinc-800 text-xl text-zinc-200 p-2 rounded-xl hover:bg-zinc-500 active:bg-zinc-400"
+                  className="flex items-center gap-2 bg-zinc-800 text-xl text-zinc-200 p-2 rounded-xl hover:bg-zinc-500 active:bg-zinc-400"
                 >
+                  <span className={materialSymbols.className}>logout</span>
                   {dictionary.logout}
                 </button>
               </form>
---
+++ app/fonts/material-symbols.ts
@@ -0,0 +1,6 @@
+import localFont from 'next/font/local'
+export const materialSymbols = localFont({
+    src: './material-symbols.woff2',
+    display: 'auto',
+    preload: true
+})
