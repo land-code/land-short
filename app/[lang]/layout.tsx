8c33c5a564c65595f2adc98c841641517ecc3487
land-code <davidgrcdiaz@gmail.com>
2023-11-08 20:51:30 +0100

    Wrap h1 inside Link instead of Link inside h1

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -29,11 +29,11 @@ export default async function RootLayout({
     <html className='h-full' lang={lang}>
       <body className={`${inter.className} h-full`}>
         <header className='flex justify-between items-center bg-zinc-800 text-white p-4'>
-          <h1 className='text-3xl'>
-            <Link href='/' className='flex items-center gap-2 bg-zinc-800 text-xl text-zinc-200 p-2 rounded-xl hover:bg-zinc-500 active:bg-zinc-400'>
+          <Link href='/' className='flex items-center gap-2 bg-zinc-800 text-xl text-zinc-200 p-2 rounded-xl hover:bg-zinc-500 active:bg-zinc-400'>
+            <h1 className='text-3xl'>
               Land shortener
-            </Link>
-          </h1>
+            </h1>
+          </Link>
           <LoginLogoutButton
             dictionary={{ login: dictionary.login, logout: dictionary.logout }} />
         </header>
