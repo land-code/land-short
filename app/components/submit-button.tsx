ffda1f0accf5a79031be97597d9b3d542c307bc0
land-code <davidgrcdiaz@gmail.com>
2023-11-08 19:05:34 +0000

    Refactor SubmitButton component to use useFormStatus hook

--- app/components/submit-button.tsx
+++ app/components/submit-button.tsx
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
-import React from "react"
 import { useFormStatus } from "react-dom"
 
 const SubmitButton = ({ children }: { children: React.ReactNode }) => {
-=======
-import { useFormStatus } from "react-dom"
-
-const SubmitButton = ({ children }: { children: JSX.Element | string }) => {
->>>>>>> main
     const { pending } = useFormStatus()
     return (
       <button
         type="submit"
         disabled={pending}
         className={pending
-<<<<<<< HEAD
           ? 'flex items-center gap-2 bg-zinc-500 text-zinc-800 p-2 rounded-xl'
           : 'flex items-center gap-2 bg-zinc-200 text-zinc-800 p-2 rounded-xl hover:bg-zinc-400 active:bg-zinc-500'
-=======
-          ? 'bg-zinc-500 text-zinc-800 p-2 rounded-xl'
-          : 'bg-zinc-200 text-zinc-800 p-2 rounded-xl hover:bg-zinc-400 active:bg-zinc-500'
->>>>>>> main
         }>
           {children}
         </button>
