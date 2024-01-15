e1f2dc3cf5401acaa87f74f066a8b03b051cbc65
David García Díaz <davidgrcdiaz@gmail.com>
2024-01-15 15:36:39 +0100

    Refactor dashboard page component

--- app/[lang]/dashboard/page.tsx
+++ app/[lang]/dashboard/page.tsx
@@ -1,14 +1,10 @@
-import { deleteLinks } from '@/app/actions'
 import { getDictionary } from '@/app/get-dictionary'
 import { Locale } from '@/app/i18n-config'
-import AddLinkButton from '@/components/add-link-button'
-import DashboardRow from '@/components/dashboard-row'
-import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { Database } from '@/database.types'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { useFormState } from 'react-dom'
+import DashboardClientPage from './dashboard.client'
 
 export default async function DashboardPage ({
   params: { lang }
@@ -31,46 +27,7 @@ export default async function DashboardPage ({
     .eq('user', user.id)
 
   if (error) console.error(error)
-  const [state, dispatch] = useFormState(deleteLinks, null)
   return (
-    <div className='p-2'>
-      <h1 className='text-3xl mb-2'>Dashboard</h1>
-      <form action={dispatch} className='grid gap-4'>
-        <div className='flex justify-between items-center'>
-          <h2 className='text-xl'>Short links</h2>
-          <div className='flex gap-2'>
-            <AddLinkButton dictionary={dictionary} language={lang} userId={user.id} />
-            <button type='submit' className='flex items-center gap-2 bg-zinc-200 text-zinc-800 p-2 rounded-full hover:bg-zinc-500 active:bg-zinc-700 dark:bg-zinc-800 dark:text-zinc-200'>
-              Delete all selected links
-            </button>
-          </div>
-        </div>
-        {state && <p className='text-zinc-700 dark:text-zinc-400'>{state}</p>}
-        <Table className='bg-zinc-300 rounded-xl p-4 dark:text-zinc-50 dark:bg-zinc-950'>
-          <TableHeader>
-            <TableRow>
-              <TableHead />
-              <TableHead className='text-zinc-700 dark:text-zinc-400'>Link</TableHead>
-              <TableHead className='text-zinc-700 dark:text-zinc-400'>Content</TableHead>
-              <TableHead className='text-zinc-700 dark:text-zinc-400'>Created at</TableHead>
-              <TableHead className='text-end text-zinc-700 dark:text-zinc-400'>Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {
-              data?.map(({ id, content, name, created_at: createdAt }) => (
-                <DashboardRow
-                  key={id}
-                  id={id}
-                  name={name}
-                  content={content}
-                  createdAt={new Date(createdAt.toString()).toLocaleDateString()}
-                />
-              ))
-            }
-          </TableBody>
-        </Table>
-      </form>
-    </div>
+    <DashboardClientPage dictionary={dictionary} data={data} lang={lang} userId={user.id} />
   )
 }
