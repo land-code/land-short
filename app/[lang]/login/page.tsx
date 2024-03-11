db01ba9665e350c408b91183b88f710bf0747f3b
David García Díaz <davidgrcdiaz@gmail.com>
2024-03-11 18:56:51 +0100

    Update login page title

--- app/[lang]/login/page.tsx
+++ app/[lang]/login/page.tsx
@@ -13,7 +13,7 @@ const Login = async ({
   const dictionary = await getDictionary(lang)
   return (
     <div className='flex flex-col items-center mt-8 justify-center'>
-      <Typography as='h2' className='mb-2'>{dictionary.saveLink}</Typography>
+      <Typography as='h2' className='mb-2'>{dictionary.login}</Typography>
       <LoginForm dictionary={dictionary.loginForm} lang={lang} />
     </div>
   )
